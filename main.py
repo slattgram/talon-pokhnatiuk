66059510d750fb1e9cac0d4506deda6a0ecf5313
slattgram <outta.hoodz@gmail.com>
2024-01-17 22:42:36 +0200

    yes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import pytest_html
 from selenium import webdriver
-from selenium.common import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
